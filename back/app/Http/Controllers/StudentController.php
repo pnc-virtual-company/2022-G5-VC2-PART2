a7cb374d2f44e934dcb0a7c4dd069bce529c53ae
Chhaiya Phai <chhaiyaphai2412@gmail.com>
2022-09-09 02:27:49 +0700

    #9 Crud student

---
+++ back/app/Http/Controllers/StudentController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Student::get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $student = new Student();
+        $student->gender = $request->gender;
+        $student->class = $request->class;
+        $student->batch = $request->batch;
+        $student->phone = $request->phone;
+        $student->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Student::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request,$id)
+    {
+        $student = Student::findOrFail($id);
+        $student->gender = $request->gender;
+        $student->class = $request->class;
+        $student->batch = $request->batch;
+        $student->phone = $request->phone;
+        $student->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Student::destroy($id);
+    }
+}
--- back/routes/api.php
+++ back/routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\StudentController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+// route students //
+Route::get("/students", [StudentController::class , 'index']);
+Route::get("/students/{id}", [StudentController::class , 'show']);
+Route::post("/students", [StudentController::class , 'store']);
+Route::post("/students/{id}", [StudentController::class , 'update']);
+Route::delete("/students/{id}", [StudentController::class ,'destroy']);
+
