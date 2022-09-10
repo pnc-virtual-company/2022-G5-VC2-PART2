58a79c2d0c98cc4d24556640f15f167da850897b
huymey <huymey.kum@student.passerellesnumeriques.org>
2022-09-10 18:57:36 +0700

    #13 create menu with active

--- front/src/components/navigation/NavbarView.vue
+++ front/src/components/navigation/NavbarView.vue
@@ -6,37 +6,34 @@
           <img src="../../assets/logo.png" alt="logo" class="w-[50px] ">
         </li>
       </ul>
-      <div class="flex">
-        <ul>
-          <li>
-            <router-link class="p-2 px-6 active:bg-orange-700" to="/dashboard">Dashboard</router-link>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <router-link class="p-2 px-6" to="/followUp">FollowUp</router-link>
-            <div class="ml-3 items-center absolute left-[48vw] top-[10px]">
-                            <small class="bg-red-400 text-white rounded-lg px-[0.30rem] border">1</small>
-                        </div>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <router-link class="p-2 px-6" to="/people">People</router-link>
-          </li>
-        </ul>
-      </div>
+      <ul class="flex">
+        <li>
+          <router-link class="p-2 px-6 " exact-active-class="exact-active-link" to="/dashboard">Dashboard</router-link>
+        </li>
+        <li>
+          <router-link class="p-2 px-6" exact-active-class="exact-active-link" to="/people">People</router-link>
+        </li>
+        <li>
+          <router-link class="p-2 px-6" exact-active-class="exact-active-link" to="/followUp">FollowUp</router-link>
+          <div class="ml-3 items-center absolute right-[44vw] top-[10px]">
+            <small class="bg-red-400 text-white rounded-lg px-[0.30rem] border">1</small>
+          </div>
+        </li>
+      </ul>
+      
       <li @click="show=!show" class="flex flex-col items-end min-w-[9rem] space-x-2 cursor-pointer">
-                    <div class="flex justify-center items-center">
-                      <router-link class="p-2 px-6" to="/profile">coordinator</router-link>
-                      <div class="w-15 h-15 ml-2">
-                          <img  src="../../assets/avatar.png" alt="" class=" w-[50px] h-[50px] rounded-full">
-                      </div>
-                      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
-                            </svg>
-                    </div>
-                </li>
+        <div class="flex justify-center items-center">
+          <router-link class="p-2 px-6" to="/profile">coordinator</router-link>
+          <div class="w-15 h-15 ml-2">
+            <img src="../../assets/avatar.png" alt="" class=" w-[50px] h-[50px] rounded-full">
+          </div>
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24"
+            stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+          </svg>
+        </div>
+      </li>
     </nav>
   </div>
 </template>
@@ -44,16 +41,16 @@
 <script>
 export default {
   data() {
-        return {
-            show: false,
-        };
-    },
+    return {
+      show: false,
+    };
+  },
 }
 </script>
 
 <style>
-/* nav a.router-link-exact-active {
+.exact-active-link {
   background: orange;
-  border-radius:4px;
-} */
+  border-radius: 4px;
+}
 </style>
