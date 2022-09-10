968e45e91f48ddf39c4c35b9e00c1c09a37d8861
huymey <huymey.kum@student.passerellesnumeriques.org>
2022-09-10 18:21:50 +0700

    #3 create Menu update

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {      
+      colors: {
+      warning: '#FF6B00',
+      primary: '#0081CA',
+      dark: '#192331',
+    },},
   },
   plugins: [],
 }
