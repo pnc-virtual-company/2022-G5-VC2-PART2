aedc3fcf1ccbc3c27058c15d99fa40a85ccfc28c
Sreykea <sreykea.reun@student.passerellesnumeriques.org>
2022-09-13 19:10:29 +0700

    #16

--- front/src/components/student/StudentList.vue
+++ front/src/components/student/StudentList.vue
@@ -22,11 +22,18 @@
       </div>
         <div class="rounded p-2 m-auto mt-4 w-full flex justify-center relative" v-if="filterStudent.length > 2" >
             <button class="flex items-center shadow p-2 px-3 rounded hover:bg-slate-200 absolute bg-white text-sm" @click="showAll"  >
-                <p v-if="showShortList">View All</p>
-                <p v-else>Hide</p>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                </svg>
+                <div v-if="showShortList" class="flex">
+                  <p class="text-sm">View All</p>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                  </svg>
+                </div>
+                <div v-else class="flex">
+                  <p class="text-sm">Hide</p>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+                  </svg>
+                </div>
             </button>   
         </div>
     </div>
