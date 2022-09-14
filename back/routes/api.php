223dbed42a326ba1daf4605a2b2197a18a936055
Chhaiya Phai <chhaiyaphai2412@gmail.com>
2022-09-15 02:59:03 +0700

    #42 Create a widget to be able to add a new batch or a new class

--- back/routes/api.php
+++ back/routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\StudentController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Public route---------======
+// Login Route
+Route::post('/login', [UserController::class, 'login']);
+// Private Route ------------=========
+// Route::group(['middleware' => ['auth:sanctum']], function() {
+    // route students //
+    Route::prefix('/students')->group(function() {
+        Route::get('/getLastStudent',[StudentController::class, 'getLastStudent']);
+    });
+    // Route User(teacher and student)
+    Route::prefix('/users')->group(function() {
+        Route::post('/',[UserController::class,'registerUser']);
+        Route::put('/{id}',[UserController::class,'update']);
+        Route::get('/students',[UserController::class,'studentOnly']);
+        Route::get('/teachers',[UserController::class,'teacherOnly']);
+        Route::get('/student/{id}',[UserController::class,'showOneStudent']);
+        Route::get('/teacher/{id}',[UserController::class,'showOneTeacher']);
+        Route::delete('/delete/{id}',[UserController::class,'destroy']);
+        Route::post('/logout',[UserController::class,'logout']);
+    });
+    // Public route to get image
+    Route::get('/storage/image/{image}', [UserController::class, 'getProfile']); /* The route to display a specific profile image */
+// });
+
--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        warning: '#FF6B00',
+        primary: '#0081CA',
+        dark: '#192331',
+      },
+    },
   },
   plugins: [],
 }
