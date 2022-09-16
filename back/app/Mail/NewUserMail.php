1a634290275ec67ed866f75a00844720691b53b8
Hinsoklim <soklim.hin@student.passerellesnumeriques.org>
2022-09-16 10:21:12 +0700

    #33 Sending Mail to user created

---
+++ back/app/Mail/NewUserMail.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class NewUserMail extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $user;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($user)
+    {
+        return $this->user = $user;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('soklimhin@gmail.com','Socail Affair')->view('mailer')->subject('Activate Account');
+    }
+}
