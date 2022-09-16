b53b53a99d3e747aa857987608127083b7c5e70b
sopheapsoeurng <sopheap.soeurng@student.passerellesnumeriques.org>
2022-09-16 17:54:54 +0700

    #51 crud class

---
+++ front/src/components/widgets/Card/CardClass.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="w-full flex mt-10">
+    <div
+      class="w-4/12 m-3 mt-10 items-center justify-center shadow rounded bg-gray-300"
+      v-for="card of classes"
+      :key="card"
+    >
+      <div
+        class="w-full item-center justify-center uppercase text-2xl font-semibold text-center p-2/12 bg-warning rounded"
+      >
+        <div class="flex justify-end text-white">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
+            />
+          </svg>
+        </div>
+        <h1 class="text-white">{{ card.name }}</h1>
+      </div>
+      <div class="text-center text-xl p-9">
+        {{ card.students.number }} Students
+        <div class="flex justify-end mt-2 text-primary">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      classes: [
+        { name: " web a", students: { number: 12 } },
+        { name: "web b", students: { number: 22 } },
+        { name: "web c", students: { number: 32 } },
+      ],
+    };
+  },
+};
+</script>
---
+++ front/src/views/Admin/dashboard/DashboardView.vue
@@ -0,0 +1,13 @@
+<template>
+    <card-class />
+</template>
+
+<script>
+import CardClass from "../../../components/widgets/Card/CardClass.vue";
+
+export default ({
+    components: {
+        "card-class": CardClass,
+    }
+})
+</script>
